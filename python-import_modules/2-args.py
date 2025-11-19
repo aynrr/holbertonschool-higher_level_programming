2c547b944df3d4f9ca785238d3f06337c6414541
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-19 09:09:22 +0000

    holberton

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    argv=sys.argv
+    argcount = len(argv) - 1
+    if argcount == 0:
+        print("0 arguments.")
+    elif argcount == 1:
+        print(1 argument:")
+    else:
+        print(f"{argcount} arguments:")
+    for a in range (1, len(argv)):
+        print(f"{a}: {argv[1]}")
