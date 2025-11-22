5e3a8eb8b2dc08bc30434c713afd5758f32a65de
root <dunyamaliyevaa007@gmail.com>
2025-11-22 19:43:55 +0000

    holberton

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    #Creating a new function that removes all characters c and C from a string.
     new_string = ""
+    #Creating a new string  that is empty 
     for i in my_string:
         if ((i != 'c')) and ((i != 'C')):
             new_string = new_string + i
