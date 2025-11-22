10e6e4f95715fdac55f81ddae81e640fa33226bd
root <dunyamaliyevaa007@gmail.com>
2025-11-22 21:08:31 +0000

    matrix and elements

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-#Creating a new function that prints matrix of integers.
-    for list in matrix:
-#At first, we create a loop that iterates over the matrix of list.
-        for element in list:
-#Then ,we create a new loop that iterates over the elements which is located inside the matrix.
-            i = list.index(element)
-            if ((i == len(list))):
-                print("{:d}".format(element), end="")
+    """
+    Print a matrix of integers.
+    """
+    for row in matrix:
+        for i in range(len(row)):
+            if i != len(row) - 1:
+                print("{:d}".format(row[i]), end=" ")
             else:
-                print("{:d}".format(element), end=" ")
-        print(end="\n")
+                print("{:d}".format(row[i]), end="")
+
+        print()
