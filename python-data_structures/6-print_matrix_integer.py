20d640a2c8361ff0d097b7746dc0d536332e6b38
root <dunyamaliyevaa007@gmail.com>
2025-11-22 21:03:35 +0000

    holberton

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,9 @@ def print_matrix_integer(matrix=[[]]):
 #Creating a new function that prints matrix of integers.
     for list in matrix:
 #At first, we create a loop that iterates over the matrix of list.
-        for i, element in enumerate(list):
+        for element in list:
 #Then ,we create a new loop that iterates over the elements which is located inside the matrix.
+            i = list.index(element)
             if ((i == len(list))):
                 print("{:d}".format(element), end="")
             else:
