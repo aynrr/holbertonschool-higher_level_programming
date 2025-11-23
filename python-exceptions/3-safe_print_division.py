5da7eb4e2bfae4290afb2e03d3bbcf688d78dfba
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-23 15:40:41 +0000

    division

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-ef safe_print_division(a, b):
+def safe_print_division(a, b):
+    try:
+        result = a/b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print ("{}".format(result))
+    return result
