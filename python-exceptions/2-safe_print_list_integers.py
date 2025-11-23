08bf996ba9f28cb057888196e50677438ce50021
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-23 15:30:54 +0000

    printing the first x elements

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for id in range (x):
+        try:
+            print("{:d}".format(my_list[id]), end="")
+            count = count + 1
+        except (ValueError, TypeError, IndexError):
+            pass
+    print()
+    return count
