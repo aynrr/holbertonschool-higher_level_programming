686d4d12921c19c0abc45de4ccc0e159111f959b
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-23 07:51:48 +0000

    printing length and first character

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    
+#Creating a function that returns a tuple with the length of a string and its first character.
+    length = len(sentence)
+    first = sentence[0]
+    if ((length == 0)):
+#If the sentence is empty, the first characte is equal to None
+        first = None
+    else:
+        return(length, first)
