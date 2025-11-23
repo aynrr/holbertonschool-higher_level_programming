eabf882a160c258792384b3681929da3492de86f
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-23 15:36:04 +0000

    holbertonschool

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for id in range (x):
+    for id in range(x):
         try:
             print("{:d}".format(my_list[id]), end="")
             count = count + 1
