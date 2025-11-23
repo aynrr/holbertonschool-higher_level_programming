5a7362d42a0d0e6a6fa4138fdba785672062d07f
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-23 08:10:10 +0000

    holberton

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if ((len(my_list) == 0)):
         return None
     else:
-        max = 0
+        max = -1
         for num in my_list:
             if num > max:
                 max = num
