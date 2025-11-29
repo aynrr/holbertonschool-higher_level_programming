7238fd4dff9a897f12ddd76270e83d2574f62fde
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-29 22:38:30 +0000

    holberton

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,5 +3,6 @@
 
 
 def write_file(filename="", text=""):
+    """opening in write mode and writing the text"""
     with open(filename, "w", encoding="utf-8") as file:
         print(file.write(text))
