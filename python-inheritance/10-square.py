dfbd4af12b388e9cd32a3d2f111792b78be40495
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-29 18:22:27 +0000

    holberton

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -9,9 +9,10 @@ class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
-        return self.__width*self.__height
+        return self.__width * self.__height
 
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__size, self.__size)
