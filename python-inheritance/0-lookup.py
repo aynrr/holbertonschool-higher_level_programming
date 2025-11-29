f02e2d7b2be7cb8fe4dd656073b24e6d4632d165
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-29 14:04:50 +0000

    holberton

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Writing a function that returns the list of available attributes and methods of an object."""
 
+
 def lookup(obj):
     """Returns the list of available attributes and methods of an object."""
     return dir(obj)
