85848092dceddac21503bee7bb67449d41d4d209
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-29 22:57:38 +0000

    holberton

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """at first we opening it"""
     with open(filename, "w", encoding="utf-8") as file:
-        return json.dumps(my_obj, file)
+        return json.dump(my_obj, file)
