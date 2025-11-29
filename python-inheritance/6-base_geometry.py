1c18f3b193e3dd376c9586938a70f62ec7364974
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-29 15:48:14 +0000

    holberton

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""creating an empty class"""
+
+
+class BaseGeometry:
+    """pass helps us to create an empty class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
