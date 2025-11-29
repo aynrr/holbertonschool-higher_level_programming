362aa44ae8ed9ba84aaa9f2f85bf3db978a92c72
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-29 15:40:46 +0000

    holberton

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""function shecks if object inherits from a class"""
+
+def inherits_from(obj, a_class):
+    """return true if obj is subclass instance of a_class"""
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
