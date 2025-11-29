18676e97610679a79868ae4416d19dd895c729a1
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-29 15:37:10 +0000

    holberton

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,5 +3,5 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """Return True if obj is an instance of a_class or its subclass, else False"""
+    """True if obj is instance of a_class/its subclass else False"""
     return isinstance(obj, a_class)
