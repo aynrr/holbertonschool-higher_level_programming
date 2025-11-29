d5df6bedf1ec44dbdd07a012e1f08e86ed12e90e
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-29 17:50:00 +0000

    holberton

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""creating an empty class"""
+
+
+class BaseGeometry:
+    """pass helps us to create an empty class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__geight = height
