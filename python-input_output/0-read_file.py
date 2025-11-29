c19dec93d600988411e5488607dbbab683bb7550
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-29 22:35:07 +0000

    holberton

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """we open it with "with" and ind read mode"""
     with open(filename, "r", encoding="utf-8") as file:
-        print(file.read(), end = "")
+        print(file.read(), end="")
