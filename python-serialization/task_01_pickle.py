ebd2d764f264ee7dd6dcec2e69468ec6992373eb
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-30 10:43:13 +0000

    holberton

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""creating class,methods and serial,deserial."""
+import picke
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Prints the object's attributes."""
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        """sserializes with picke"""
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+        except Exception:
+            return None
+
+ @classmethod
+    def deserialize(cls, filename):
+        """deserializes with pickle"""
+        try:
+            with open(filename, "rb") as f:
+                obj = pickle.load(f)
+                return obj
+        except Exception:
+            return None
