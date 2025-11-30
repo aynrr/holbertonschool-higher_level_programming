2ef82e4c94ceb28eb222b4c11e048197fba0bd61
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-30 15:51:44 +0000

    holberton

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,3 @@
 -- a script that display records
-SELECT score, name FROM second_table WHERE name IS NOT NULL AND name !='' AND name != 'Bob' ORDER BY score DESC;
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
+
