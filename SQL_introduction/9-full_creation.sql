4d347e338f61c86367e924b55537435b93f23e23
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-30 15:25:53 +0000

    holberton

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,10 @@
 -- creating a table and adding many rows
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
-    name VARCHAR(256)
+    name VARCHAR(256),
     score INT
 );
+
 INSERT INTO second_table (id, name, score)
 VALUES
 (1, 'JOHN', 10),
