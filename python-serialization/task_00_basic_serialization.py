6e8eee4f5ec644984f41804639bb52808ddf23ed
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-30 10:34:52 +0000

    holberton

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -11,4 +11,4 @@ def serialize_and_save_to_file(data, filename):
 def load_and_deserialize(filename):
     """loads and deserializates"""
     with open(filename, "r", encoding="utf-8") as file:
-        json.load(file)
+        return json.load(file)
