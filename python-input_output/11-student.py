c5b2ea08d7ca51797519a50975e3f2baa4ea3d25
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-30 09:35:31 +0000

    holberton

---
+++ python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" a comment"""
+
+
+class Student:
+    """at first we create a class"""
+    def __init__(self, first_name, last_name, age):
+        """creating public instance attr."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves the dict."""
+        if isinstance(attrs, list):
+            new_dict = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    new_dict[key] = self.__dict__[key]
+            return new_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """comment"""
+        for key, value in json.items():
+            setattr(self, key, value)
