967ab1c0683bc7beedb732328383932efa552b24
Aynur <dunyamaliyevaa007@gmail.com>
2025-11-30 08:21:46 +0000

    holberton

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,12 +3,16 @@
 import sys
 
 
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 file = "add_item.json"
+
 try:
     a = load_from_json_file(file)
 except:
     a = []
+
 a.extend(sys.argv(1:)
+
 save_to_json_file(a, file)
