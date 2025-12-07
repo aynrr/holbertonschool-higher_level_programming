0bd5ccd14c247fcd68e9d54e6c6e6e68341a4012
Aynur <dunyamaliyevaa007@gmail.com>
2025-12-07 10:27:46 +0000

    my first request

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import urllib.request
+url = "https://intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as response:
+    body = response.read()decode("utf-8")
+    print("\t" + body)
