8cac54e1c5ff695d20263dc2da1b87034d1c11cd
Aynur <dunyamaliyevaa007@gmail.com>
2025-12-07 10:33:42 +0000

    holberton

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status using urllib"""
+
 import urllib.request
-url = "https://intranet.hbtn.io/status"
-with urllib.request.urlopen(url) as response:
-    body = response.read().decode("utf-8")
-    print("\t" + body)
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
